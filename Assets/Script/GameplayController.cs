d7f238d4a0a954c70b39f3ed98f896e795e17b2d
Rashid Massanchi <rashid.massanchi@gmail.com>
2023-06-06 01:22:17 +0600

    Submitting Tower Blocks game

---
+++ Assets/Script/GameplayController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public static GameplayController instance;
+
+    public BoxSpawner box_Spawner;
+
+    [HideInInspector]
+    public BoxScript currentBox;
+
+    public CameraFollow cameraScript;
+    private int moveCount;
+
+    void Awake(){
+        if (instance ==null)
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box_Spawner.SpawnBox();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+    
+    void DetectInput(){
+        if (Input.GetMouseButtonDown(0)){
+            currentBox.DropBox();
+        }
+    }
+
+    public void SpawnNewBox(){
+        Invoke("NewBox", 0f);
+    }
+
+    void NewBox(){
+        box_Spawner.SpawnBox();
+    }
+
+    public void MoveCamera(){
+        moveCount ++;
+
+        if (moveCount == 3){
+            moveCount = 0;
+            cameraScript.targetPos.y += 2f;
+
+        }
+
+
+    }
+
+    public void RestartGame(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+    
+}
+
+
+
+
